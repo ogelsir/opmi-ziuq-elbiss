2310f8a0849848f499221c7caa4772edada32faa
Felix-Yann <felixyan2001@hotmail.com>
2017-05-03 19:45:31 -0700

    Template

---
+++ Ideas.java
@@ -0,0 +1,18 @@
+public class Ideas
+{    
+    INSTRUCTIONS(){
+        /**
+         * Get your ideas out, write different kinds of them in different "methods"
+         */
+    }
+    MINIGAMES(){
+        /**
+         * Write mini game ideas here?
+         */
+    }
+    TWEAKS(){
+        /**
+         * Write tweaks that screw up the player experience here
+         */
+    }
+}
