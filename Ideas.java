e939258baf7a6f0cb82c2a41b93471fca0c37e02
Felix-Yann <felixyan2001@hotmail.com>
2017-05-04 08:53:06 -0700

    Update Ideas.java

--- Ideas.java
+++ Ideas.java
@@ -5,14 +5,32 @@ public class Ideas
          * Get your ideas out, write different kinds of them in different "methods"
          */
     }
+    IMPLEMENTATION(){
+        /**
+         * Probably have an Arraylist which has the same amount of numbers (1-10) for each game class, shuffled randomly
+         * for a random order of "games"
+         * Once the list reaches size 0 run an end game level class
+         * Three booleans that are checked every time to represent lives, drawn on the screen in the top right
+         */
+    }
     MINIGAMES(){
         /**
          * Write mini game ideas here?
+         * A riddle
+         * Math? True/False, Type the Answer
+         * 
          */
     }
     TWEAKS(){
         /**
          * Write tweaks that screw up the player experience here
+         * Riddle: Time limit, riddle is printed backwards,
+         * Math: Time limit, difficulty of problem up,
+         */
+    }
+    BOSSFIGHT(){
+        /**
+         * Put ideas for last level
          */
     }
 }
