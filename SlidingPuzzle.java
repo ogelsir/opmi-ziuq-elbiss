c142983c31d3cc4ef9d153b40e1702f163d000db
Oliver Jiang <ogelsir@gmai.com>
2017-05-17 12:27:17 -0700

    added Tile and SlidingPuzzle classes
    finished Tile class. no comments yet.

---
+++ SlidingPuzzle.java
@@ -0,0 +1,13 @@
+
+/**
+ * data class for a sliding puzzle
+ * ex: https://en.wikipedia.org/wiki/Sliding_puzzle
+ * probably for a lock?
+ * 
+ * @author Oliver Jiang
+ * @version 5/17/17
+ */
+public class SlidingPuzzle
+{
+    
+}
---
+++ Tile.java
@@ -0,0 +1,13 @@
+
+/**
+ * Tiles for the SlidingPuzzle
+ * 
+ * @author Oliver Jiang
+ * @version 5/17/17
+ */
+public interface Tile
+{
+    public void draw();
+    public int getStartRow();
+    public int getStartCol();
+}
